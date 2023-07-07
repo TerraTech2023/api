4bf809662dfd7258a042e890fbd1a5c026327a81
Jonatas <delimajonatas875@gmail.com>
2023-07-07 14:54:55 -0300

    commit inicial

---
+++ src/main/java/com/terratech/api/ApiTerraTechApplication.java
@@ -0,0 +1,13 @@
+package com.terratech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTerraTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTerraTechApplication.class, args);
+    }
+
+}
