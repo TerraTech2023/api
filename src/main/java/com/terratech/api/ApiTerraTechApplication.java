f29ab3a9d1ac059d1d40270a4725718ac964c595
Jonatas <delimajonatas875@gmail.com>
2023-07-11 11:36:58 -0300

    criado rota de informações da api

--- src/main/java/com/terratech/api/ApiTerraTechApplication.java
+++ src/main/java/com/terratech/api/ApiTerraTechApplication.java
@@ -2,12 +2,28 @@ package com.terratech.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
 @SpringBootApplication
 public class ApiTerraTechApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiTerraTechApplication.class, args);
+    }
+
+    @GetMapping
+    public Map<String, String> hello() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "TerraTech");
+        map.put("version", "1.0-alpha");
+        map.put("description", "API TerraTech");
+        return map;
     }
 
 }
